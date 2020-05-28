73a1bb84a5dfd5590b6697cd0fda1fc8c08dfddc
Alexander <sidorovalexander1992@gmail.com>
2020-05-28 15:36:04 +0300

    Добавил ProfileController

---
+++ src/main/java/com/kngpsk/controllers/ProfileController.java
@@ -0,0 +1,23 @@
+package com.kngpsk.controllers;
+
+import com.kngpsk.domain.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile/{user}")
+    public String profileUser(@PathVariable User user,
+                              @AuthenticationPrincipal User currentUser,
+                              Model model){
+        model.addAttribute("userChanel",user);
+        model.addAttribute("isCurrentUser",user.equals(currentUser));
+
+        return "profile";
+
+    }
+}
