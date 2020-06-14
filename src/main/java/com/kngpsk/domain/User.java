4879df5b8620fb36df2eeda34be3f130f80ef394
Alexander <sidorovalexander1992@gmail.com>
2020-06-14 16:26:09 +0300

    Добавил валидацию данных юзера при вводе пароля и регистрации

--- src/main/java/com/kngpsk/domain/User.java
+++ src/main/java/com/kngpsk/domain/User.java
@@ -5,6 +5,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Objects;
@@ -17,14 +19,14 @@ public class User implements UserDetails, Serializable, FileSetter {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //@NotBlank(message = "Username cannot be empty.")
+    @NotBlank(message = "Username cannot be empty.")
     private String username;
-    //@NotBlank(message = "Password cannot be empty.")
+    @NotBlank(message = "Password cannot be empty.")
     private String password;
     private boolean active;
 
-//    @Email(message = "Email is not correct")
-//    @NotBlank(message = "Email cannot be empty.")
+    @Email(message = "Email is not correct")
+    @NotBlank(message = "Email cannot be empty.")
     private String email;
 
     private String activationCode;
